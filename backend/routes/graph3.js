9158acb52c65f62554467e815bdc682ced2e11a7
Xixiang-Liu <xixiang3@illinois.edu>
2023-11-29 01:01:25 -0600

    make graph3.js more readable

--- backend/routes/graph3.js
+++ backend/routes/graph3.js
@@ -3,16 +3,24 @@ var router = express.Router();
 var db = require('../db');
 
 // my version of query: wrap db.query in a promise
+// terminal doesn't allow me to put await before something that's not a promise
 function query(sql) {
     return new Promise((resolve, reject) => {
-        db.query(sql, (err, results) => {
-            if (err) reject(err);
-            else resolve(results);
+        // though we check err in the main endpoint, not sure if we need to check it here
+        // so check it here as well to be safe
+        db.query(sql, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(result);
+            }
         });
     });
 }
 
 router.get('/', async function(req, res) {
+    // doc says area, which should mean Division (a string)
     const Division = req.query.Division;
 
     // async typically use try catch, not then
@@ -25,14 +33,18 @@ router.get('/', async function(req, res) {
                           NATURAL JOIN District
                           JOIN Record USING (DistrictId)
                      WHERE Division = '${Division}'`;
+        // use the promise version
+        // db.query can't be behind the await, terminal doesn't allow me to do so
         let dateResult = await query(sql);
 
+        // not sure if a dict is ok also
         var month_count = new Map();        
 
         // count crimNum of each month
         for (i = 0; i < dateResult.length; i++) {
             date = dateResult[i].DateOcc.toString();
             // see how toString works for date object
+            // the format change after conversion from date to string
             month = date.substr(4, 3);
 
             if (month_count.has(month)) {
