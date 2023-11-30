0dfca71ae4ddfb0d9c8b502ce96c283be9cf39fe
tiansuc2 <tiansuc2@illinois.edu>
2023-11-30 00:51:14 -0600

    Add CORS

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 app.use('/', indexRouter);
 app.use('/crime', crimeRouter);
 app.use('/premise', premiseRouter);
