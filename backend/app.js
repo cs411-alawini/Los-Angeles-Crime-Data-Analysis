087d10407abfdbfe13d25723fbaf5caae5957fc1
Xixiang-Liu <xixiang3@illinois.edu>
2023-12-03 23:50:33 -0600

    remove graph2.js and graph3.js

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,7 @@ var premiseRouter = require('./routes/premise');
 var weaponRouter = require('./routes/weapon');
 var recordRouter = require('./routes/record');
 var graph1Router = require('./routes/graph1');
-var graph2Router = require('./routes/graph2');
-var graph3Router = require('./routes/graph3');
+var graph2and3Router = require('./routes/graph2&3');
 
 var app = express();
 
@@ -40,8 +39,7 @@ app.use('/premise', premiseRouter);
 app.use('/weapon', weaponRouter);
 app.use('/record', recordRouter);
 app.use('/graph1', graph1Router);
-app.use('/graph2', graph2Router);
-app.use('/graph3', graph3Router);
+app.use('/graph2&3', graph2and3Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
